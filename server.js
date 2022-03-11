cd5aba88c5a03436cc843d4457ef4930d6e7c12b
John Tannous <john.tannous97@gmail.com>
2022-03-10 20:29:57 -0800

    updated code for tech

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
+
 const hbs = exphbs.create({ helpers });
 
 const sess = {
@@ -27,7 +27,7 @@ const sess = {
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
